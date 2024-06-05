e6dfe0c21464c67ec89649c2616f97658fb8414e
Maria Eduarda Cruz <161530778+madu2905@users.noreply.github.com>
2024-06-05 18:55:49 -0300

    adicionado pagina de login

---
+++ java/script.js
@@ -0,0 +1,7 @@
+function logarContaGoogle() {
+  window.location.href = "https://mail.google.com/mail/u/0/#inbox";  // Redireciona para o Gmail, por exemplo.
+}
+
+function redirecionarGoogle() {
+  logarContaGoogle();  // Redireciona diretamente para o login com Google
+}
